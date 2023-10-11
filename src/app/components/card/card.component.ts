43069e89c97c3cb0ea419f43100353bcc5bb81a1
mateuslgomes <mateuslgomes.dev@gmail.com>
2023-10-11 09:55:22 -0300

    adding games and passing parameters

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  @Input()
+  gameCover: string = '';
 
-  constructor() { }
+  @Input()
+  gameLabel: string = '';
 
-  ngOnInit(): void {
-  }
+  @Input()
+  gameType: string = 'Digital PS4';
 
+  @Input()
+  gamePrice: string = 'R$ 399,90';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
